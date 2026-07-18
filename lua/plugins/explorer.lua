e5968ceb0eebcffc40f6a935a23882235e6adee0
Adibayu Luthfiansyah <adibayuluthfiansyah@gmail.com>
2026-07-19 00:05:00 +0700

    nvim config

---
+++ lua/plugins/explorer.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {},
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+          git_status = true,
+          git_status_open = true,
+        },
+      },
+    },
+  },
+}
---
+++ lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/adibayu/.config/omarchy/current/theme/neovim.lua
